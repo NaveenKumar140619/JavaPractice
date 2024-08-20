c161a7da41e7f85612ebedde707c4fbbcfb0c8b9
Naveen Kumar <naveensmart680@gmail.com>
2024-08-20 18:03:27 +0000

    push code

---
+++ FindArea.java
@@ -0,0 +1,7 @@
+public class FindArea {
+    public static void main(String[] args) {
+        int b = 15, h = 7;
+        double a = 0.5*b*h;
+        System.out.println(a);
+    }
+}
---
+++ SumofVariables.java
@@ -0,0 +1,8 @@
+public class SumofVariables {
+    public static void main(String[] args) {
+        int variable1 = 23;
+        int variable2 = 27;
+        int result = (variable1 + variable2);
+        System.out.println( result);
+    }
+}
