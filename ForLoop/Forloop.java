485679cc6f16218ac910dc2ba1cead51b93cd754
Naveen Kumar <naveensmart680@gmail.com>
2024-08-23 21:22:29 +0000

    push code

---
+++ ForLoop/Forloop.java
@@ -0,0 +1,21 @@
+package ForLoop;
+
+public class Forloop {
+        public static void main(String[] args) {
+          // Write a for loop that prints out the phrase "I love Coding" 5 times
+      for(int i=0; i<5; i++ ){
+        System.out.println("I Love Coding");
+      }
+          // Write a for loop that prints out the numbers 1 to 10
+      for(int i=1; i<=10; i++){
+        System.out.println(i);
+      }
+          // Write a for loop that prints out the numbers 10 to 1
+          for(int i=10; i>=1; i--){
+            System.out.println(i);
+          }
+           
+      
+        }
+      
+      }
